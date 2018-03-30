a89755753070c8614b36964edbee3c7dfb6338e3
Luis Pedro Coelho <luis@luispedro.org>
2018-03-30 12:38:45 +0200

    ENH Split gut processing into separate blocks

---
+++ ngless/README.md
@@ -0,0 +1,21 @@
+# NGLess scripts
+
+Note that the results of running this benchmark are provided in
+`data/precomputed`.
+
+You must have jug and ngless installed and have run the `download-data.py`
+script at the top level of this repository.
+
+Running:
+
+    jug execute
+
+will generate the file `ngless_benchmark.csv` with the results.
+
+The files `gut.ngl` and `ocean.ngl` contain the full pipelines. The variations
+with names such as `gut0-rtf.ngl` contain parts of the processing. The only
+purpose of splitting the pipeline into its constituent parts is to facilitate
+benchmarking.
+
+
+
--- ngless/jugfile.py
+++ ngless/jugfile.py
@@ -1,36 +1,61 @@
 from jug import TaskGenerator, barrier
 
 @TaskGenerator
-def run_time(script, ncpu, tag):
-    '''
-
-    Argument tag is ignored, but is necessary to distinguish the different runs
-    of the script.
-
-    '''
+def run_time(target, ncpu, _):
+    print("RUNNING {}".format(target))
     import subprocess
-    args = ['/usr/bin/time', '--verbose', 'ngless', '-j', str(ncpu), '--trace', script]
-    p = subprocess.run(args,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            check=True)
-    return (p.stdout, p.stderr)
+    args = ['/usr/bin/time', '--verbose', 'ngless', '--trace', target, '-j', str(ncpu)]
+    try:
+        p = subprocess.run(args,
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE,
+                           check=True)
+    except subprocess.CalledProcessError as e:
+        print(e.stdout)
+        print(e.stderr)
+        raise
+    else:
+        return (p.stdout, p.stderr)
+
 
 @TaskGenerator
-def cleanup_ngless():
+def cleanup_ngless(_):
     import shutil
     shutil.rmtree('ngless-locks', ignore_errors=True)
     shutil.rmtree('ngless-partials', ignore_errors=True)
     shutil.rmtree('ngless-stats', ignore_errors=True)
+    shutil.rmtree('gut-temp', ignore_errors=True)
+    shutil.rmtree('ocean-temp', ignore_errors=True)
 
-NCPU=8
-NREPLICATES=3
+
+@TaskGenerator
+def make_temp_directories(_):
+    import os
+    os.makedirs('gut-temp/screen-igc')
+    os.makedirs('gut-temp/filtered-igc')
+    for s in os.listdir('../data/gut/'):
+        if not s.startswith('SAME'): continue
+        os.makedirs(f'gut-temp/rtf-{s}')
+        os.makedirs(f'gut-temp/screen-filter-hg19-{s}')
+
+NCPU = 8
+NREPLICATES = 3
+NSAMPLES = 3
 
 outs = {}
 for rep in range(NREPLICATES):
-    cleanup_ngless()
-    for sc in ['ocean.ngl', 'gut.ngl']:
-        for i in range(3):
-            outs[sc, rep, i] = run_time(sc, NCPU, rep*NREPLICATES + i)
+    cleanup_ngless(rep)
+    make_temp_directories(rep)
+    for target in [ 'gut0-rtf.ngl',
+                    'gut1-s.hg19.ngl',
+                    'gut2-f.hg19.ngl',
+                    'gut3-motus.ngl',
+                    'gut4-s.igc.ngl',
+                    'gut5-f.igc.ngl',
+                    'gut6-p.igc.ngl',
+                    'gut-full.ngl',
+                        ]:
+        for i in range(NSAMPLES):
+            outs[target, rep, i] = run_time(target, NCPU, rep*NREPLICATES + i)
     barrier()
 
