d4ffe271d4ea26286e3a043c3ebe8a5941e1f3b9
Renato Alves <alves.rjc@gmail.com>
2018-04-05 19:09:54 +0200

    MNT Rename databases/catalogs to match public names

--- mocat/run_mocat_gut.sh
+++ mocat/run_mocat_gut.sh
@@ -19,29 +19,29 @@ TIMING=timing-individual
 
 # Mapping to 10M gene catalog
 /usr/bin/time --verbose -ao $TIMING MOCAT.pl -cfg MOCAT.cfg -sf $SAMPLEFILE -cpus "$NCPU" \
-    -s 1000RefGeneCat.1 1000RefGeneCat.2 -r reads.processed -extracted_files \
-    &>> logs/screen-1000RefGeneCat_${SAMPLEFILE}.log
+    -s IGC.1 IGC.2 -r reads.processed -extracted_files \
+    &>> logs/screen-IGC_${SAMPLEFILE}.log
 
 /usr/bin/time --verbose -ao $TIMING MOCAT.pl -cfg MOCAT.cfg -sf $SAMPLEFILE -cpus "$NCPU" \
-    -f 1000RefGeneCat.1 1000RefGeneCat.2 -r reads.processed -config filter_psort_buffer=8G -memory 20G \
-    &>> logs/screen-1000RefGeneCat_${SAMPLEFILE}.log
+    -f IGC.1 IGC.2 -r reads.processed -config filter_psort_buffer=8G -memory 20G \
+    &>> logs/screen-IGC_${SAMPLEFILE}.log
 
 /usr/bin/time --verbose -ao $TIMING MOCAT.pl -cfg MOCAT.cfg -sf $SAMPLEFILE -cpus "$NCPU" \
-    -p 1000RefGeneCat.1 1000RefGeneCat.2 -r reads.processed -mode functional \
+    -p IGC.1 IGC.2 -r reads.processed -mode functional \
     -no_paste -no_horizontal -memory 3G \
-    &>> logs/screen-1000RefGeneCat_${SAMPLEFILE}.log
+    &>> logs/screen-IGC_${SAMPLEFILE}.log
 
 
 # Below SpecI
 /usr/bin/time --verbose -ao $TIMING MOCAT.pl -cfg MOCAT.cfg -sf $SAMPLEFILE -cpus "$NCPU" \
-    -s 263MetaRef10MGv9.cal.v2.nr.padded -r hg19 -identity 97 -extracted_files \
+    -s mOTU.v1.padded -r hg19 -identity 97 -extracted_files \
     &>> logs/screen_motus_${SAMPLEFILE}.log
 
 /usr/bin/time --verbose -ao $TIMING MOCAT.pl -cfg MOCAT.cfg -sf $SAMPLEFILE -cpus "$NCPU" \
-    -f 263MetaRef10MGv9.cal.v2.nr.padded -r hg19 -identity 97 -config filter_psort_buffer=2G -memory 5G \
+    -f mOTU.v1.padded -r hg19 -identity 97 -config filter_psort_buffer=2G -memory 5G \
     &>> logs/filter_motus_${SAMPLEFILE}.log
 
 /usr/bin/time --verbose -ao $TIMING MOCAT.pl -cfg MOCAT.cfg -sf $SAMPLEFILE -cpus "$NCPU" \
-    -p 263MetaRef10MGv9.cal.v2.nr.padded -r hg19 -identity 97 -mode mOTU \
+    -p mOTU.v1.padded -r hg19 -identity 97 -mode mOTU \
     -no_paste -no_horizontal -memory 3G \
     &>> logs/profile_motus_${SAMPLEFILE}.log
