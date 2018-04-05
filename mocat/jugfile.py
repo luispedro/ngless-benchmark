8e461cbea4160d07c9bbf57469a5278fbf4c28b6
Renato Alves <alves.rjc@gmail.com>
2018-04-05 18:10:49 +0200

    MNT Merge postproc-mocat with jugfile for simplicity

--- mocat/jugfile.py
+++ mocat/jugfile.py
@@ -5,6 +5,7 @@ import re
 import shutil
 from jug import TaskGenerator, barrier
 from glob import glob
+import pandas as pd
 
 
 @TaskGenerator
@@ -61,6 +62,57 @@ def flush_logs():
     os.mkdir("logs")
 
 
+@TaskGenerator
+def process_results():
+    data = []
+
+    with open("timing-individual") as fh:
+        row = {}
+        for line in fh:
+            if line.strip().startswith("Command exited with non-zero"):
+                continue
+
+            name, value = line.strip().split(": ")
+            value = value.strip('"')
+
+            if name == "Elapsed (wall clock) time (h:mm:ss or m:ss)":
+                if '.' in value:
+                    value = value.split(":")
+                    value[-1] = round(float(value[-1]))
+                else:
+                    value = value.split(":")
+
+                if len(value) == 2:
+                    seconds = int(value[0]) * 60 + int(value[1])
+                elif len(value) == 3:
+                    seconds = int(value[0]) * 3600 + int(value[1]) * 60 + int(value[2])
+                else:
+                    raise Exception("Unknown time format {}".format(value))
+                value = seconds
+
+            row[name] = value
+
+            if name == "Exit status":
+                data.append(row)
+                row = {}
+
+    d = pd.DataFrame(data)
+    d["Command being timed"] = d["Command being timed"].str.replace("MOCAT.pl -cfg MOCAT.cfg -sf ", "")
+    d["Command being timed"] = d["Command being timed"].str.replace(" -cpus 8", "")
+    d['dataset'], d["Command being timed"] = d["Command being timed"].str.split(' ', 1).str
+    d['action'], d["Command being timed"] = d["Command being timed"].str.split(' ', 1).str
+    d.loc[d["Command being timed"] == "-config", "Command being timed"] = " "
+    d['target'], d["Command being timed"] = d["Command being timed"].str.split(' ', 1).str
+
+    d.loc[d["action"] == "-rtf", "action"] = "ReadTrimFilter"
+    d.loc[d["action"] == "-s", "action"] = "Screen"
+    d.loc[d["action"] == "-p", "action"] = "Profile"
+    d.loc[d["action"] == "-f", "action"] = "Filter"
+    d.rename({"Elapsed (wall clock) time (h:mm:ss or m:ss)": "Elapsed time"}, axis="columns", inplace=True)
+
+    d.to_csv("mocat_benchmark.csv")
+
+
 NCPU = 8
 NREPLICATES = 3
 
@@ -74,3 +126,5 @@ for rep in range(NREPLICATES):
         outs[target, rep] = time
         remove_data_and_results(time)
         barrier()
+
+process_results()
