88dc388b32f86dbe92e4c6ecd14936bf255adb54
Renato Alves <alves.rjc@gmail.com>
2018-04-05 19:09:54 +0200

    MNT Rename databases/catalogs to match public names

--- mocat/run_mocat_tara.sh
+++ mocat/run_mocat_tara.sh
@@ -14,39 +14,35 @@ TIMING=timing-individual
 
 # Mapping to ocean catalog
 /usr/bin/time --verbose -ao $TIMING MOCAT.pl -cfg MOCAT.cfg -sf $SAMPLEFILE -cpus "$NCPU" \
-    -s GORGeCat.95.padded.1 GORGeCat.95.padded.2 GORGeCat.95.padded.3 GORGeCat.95.padded.4 \
-       GORGeCat.95.padded.5 GORGeCat.95.padded.6 GORGeCat.95.padded.7 GORGeCat.95.padded.8 \
+    -s OM-RGC.1 OM-RGC.2 OM-RGC.3 OM-RGC.4 OM-RGC.5 OM-RGC.6 OM-RGC.7 OM-RGC.8 \
     -r reads.processed -extracted_files -memory 60G \
-    &>> logs/screen-GORGeCat_${SAMPLEFILE}.log
+    &>> logs/screen-OM-RGC_${SAMPLEFILE}.log
 
 /usr/bin/time --verbose -ao $TIMING MOCAT.pl -cfg MOCAT.cfg -sf $SAMPLEFILE -cpus "$NCPU" \
-    -f GORGeCat.95.padded.1 GORGeCat.95.padded.2 GORGeCat.95.padded.3 GORGeCat.95.padded.4 \
-       GORGeCat.95.padded.5 GORGeCat.95.padded.6 GORGeCat.95.padded.7 GORGeCat.95.padded.8 \
+    -f OM-RGC.1 OM-RGC.2 OM-RGC.3 OM-RGC.4 OM-RGC.5 OM-RGC.6 OM-RGC.7 OM-RGC.8 \
     -r reads.processed -config filter_psort_buffer=8G -memory 60G \
-    &>> logs/filter-GORGeCat_${SAMPLEFILE}.log
+    &>> logs/filter-OM-RGC_${SAMPLEFILE}.log
 
 /usr/bin/time --verbose -ao $TIMING MOCAT.pl -cfg MOCAT.cfg -sf $SAMPLEFILE -cpus "$NCPU" \
-    -p GORGeCat.95.padded.1 GORGeCat.95.padded.2 GORGeCat.95.padded.3 GORGeCat.95.padded.4 \
-       GORGeCat.95.padded.5 GORGeCat.95.padded.6 GORGeCat.95.padded.7 GORGeCat.95.padded.8 \
+    -p OM-RGC.1 OM-RGC.2 OM-RGC.3 OM-RGC.4 OM-RGC.5 OM-RGC.6 OM-RGC.7 OM-RGC.8 \
     -r reads.processed -mode functional \
     -no_paste -no_horizontal -memory 200G \
-    &>> logs/profile-GORGeCat_${SAMPLEFILE}.log
+    &>> logs/profile-OM-RGC_${SAMPLEFILE}.log
 
 # Check MOCAT.cfg for which categories to do with functional profiling
 # KEGG (kegg exists as ko, pathway, modules in the functional.map file) - doing ko only
-# TODO how to do eggnog functional profiling? (eggnog isn't in the functional.map file)?
 
 # Below SpecI
 /usr/bin/time --verbose -ao $TIMING MOCAT.pl -cfg MOCAT.cfg -sf $SAMPLEFILE -cpus "$NCPU" \
-    -s Ref10MGv9.padded -r reads.processed -extracted_files \
+    -s RefMG.v1.padded -r reads.processed -extracted_files \
     -no_paste -no_horizontal -memory 3G \
     &>> logs/screen-refmg_${SAMPLEFILE}.log
 
 /usr/bin/time --verbose -ao $TIMING MOCAT.pl -cfg MOCAT.cfg -sf $SAMPLEFILE -cpus "$NCPU" \
-    -f Ref10MGv9.padded -r reads.processed -config filter_psort_buffer=8G -memory 20G \
+    -f RefMG.v1.padded -r reads.processed -config filter_psort_buffer=8G -memory 20G \
     &>> logs/filter-refmg_${SAMPLEFILE}.log
 
 /usr/bin/time --verbose -ao $TIMING MOCAT.pl -cfg MOCAT.cfg -sf $SAMPLEFILE -cpus "$NCPU" \
-    -p Ref10MGv9.padded -r reads.processed -mode gene \
+    -p RefMG.v1.padded -r reads.processed -mode gene \
     -no_paste -no_horizontal -memory 3G \
     &>> logs/profile-refmg_${SAMPLEFILE}.log
