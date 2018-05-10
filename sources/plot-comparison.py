33bb8fc6931afa1675cdfdd6c86b9a2e457279b1
Luis Pedro Coelho <luis@luispedro.org>
2018-05-10 15:02:00 +0200

    ENH Plot comparison output

--- sources/plot-comparison.py
+++ sources/plot-comparison.py
@@ -3,6 +3,16 @@ import seaborn as sns
 
 ngl = pd.read_table('../data/precomputed/ngless_benchmarks.tsv')
 mocat = pd.read_csv('../data/precomputed/mocat_benchmark.csv', index_col=0)
+hts = pd.read_table('../data/precomputed/htseq-count_benchmark.tsv')
+hts.rename(columns={
+    hts.columns[0]: 'dataset',
+    hts.columns[1]: 'action',
+    }, inplace=True)
+
+hts = hts.groupby(['dataset', 'action', 'Unnamed: 3']).sum().reset_index()
+hts['action'] = hts.action.map({'map':'Screen', 'htseq-count':'Profile'})
+hts['tool'] = 'bwa/htseq-count'
+hts['target'] = hts.dataset.map({'gut': 'IGC', 'tara' : 'OM-RGC'})
 
 mocat.rename(columns={
     'Elapsed time': 'wallclock',
@@ -11,6 +21,7 @@ mocat.rename(columns={
 
 mocat = mocat[~ (mocat.target == 'RefMG.v1.padded')]
 mocat = mocat[~ (mocat.target == 'mOTU.v1.padded')]
+mocat = mocat[~ (mocat.target == 'hg19')]
 
 
 command = {
@@ -34,14 +45,19 @@ ngl['dataset'] = ngl.command.map(lambda c: command[c.split()[2]][0])
 ngl['action'] = ngl.command.map(lambda c: command[c.split()[2]][1])
 ngl['target'] = ngl.command.map(lambda c: command[c.split()[2]][2])
 
-ngl = ngl.groupby([ngl.columns[0], ngl.columns[2]]).sum()
+ngl = ngl.groupby([ngl.columns[0], ngl.columns[1]]).sum()
 ngl['tool'] = 'ngless'
 mocat['tool'] = 'mocat'
 ngl['dataset'] = ngl.index.get_level_values(0).map(lambda c: command[c][0])
 ngl['action'] = ngl.index.get_level_values(0).map(lambda c: command[c][1])
 ngl['target'] = ngl.index.get_level_values(0).map(lambda c: command[c][2])
 
-ngl = ngl.drop(['ocean4-p-seqname-om-rgc.ngl', 'gut3-motus.ngl'])
+ngl = ngl.drop([
+        'ocean4-p-seqname-om-rgc.ngl',
+        'gut3-motus.ngl',
+        'gut1-s.hg19.ngl',
+        'gut2-f.hg19.ngl',
+        ])
 
 COLS = ['dataset', 'action', 'target', 'tool', 'wallclock']
 
@@ -51,15 +67,17 @@ tara_full = pd.DataFrame([('tara', 'Full', '-', 'mocat', wc) for wc in tara_full
 gut_full = mocat[mocat.dataset == 'gut'].reset_index()
 gut_full = pd.DataFrame([('gut', 'Full', '-', 'mocat', wc) for wc in gut_full.groupby(lambda i : i // 5).sum()['wallclock'].values], columns=COLS)
 
-data = pd.concat((ngl[COLS], mocat[COLS], gut_full, tara_full))
+data = pd.concat((ngl[COLS], mocat[COLS], hts[COLS], gut_full, tara_full))
 
 data.reset_index(inplace=True)
 data.fillna('-', inplace=True)
 
-g = sns.factorplot(x="action", y="wallclock", hue="tool", hue_order=['ngless', 'mocat'], kind="box", col='dataset', data=data, aspect=1.7)
+g = sns.factorplot(x="action", y="wallclock", hue="tool", hue_order=['ngless', 'mocat', 'bwa/htseq-count'], kind="box", row='dataset', data=data, aspect=1.7)
 g.fig.get_axes()[0].set_yscale('log')
 g.fig.get_axes()[1].set_yscale('log')
 
 g.fig.tight_layout()
-g.savefig('ngless-mocat-compare.pdf')
+g.savefig('ngless-mocat-htseq-count-compare.png', dpi=150)
+g.savefig('ngless-mocat-htseq-count-compare.svg')
+g.savefig('ngless-mocat-htseq-count-compare.pdf')
 
