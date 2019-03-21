e1408a7d08b5b2a1e4dd6e8789128169ecfd0576
Luis Pedro Coelho <luis@luispedro.org>
2019-03-21 04:45:12 +0100

    ENH More extensive benchmarking

--- ngless/jugfile.py
+++ ngless/jugfile.py
@@ -2,7 +2,7 @@ from jug import TaskGenerator, barrier
 
 @TaskGenerator
 def run_time(target, ncpu, _):
-    print("RUNNING {}".format(target))
+    print("RUNNING {} on {} CPUS".format(target, ncpu))
     import subprocess
     args = ['/usr/bin/time', '--verbose', 'ngless', '--trace', target, '-j', str(ncpu)]
     try:
@@ -56,9 +56,29 @@ def save_to(outs, oname):
     import pandas as pd
     pd.DataFrame(outs).T.to_csv(oname, sep='\t')
 
+@TaskGenerator
+def summarize(outs_cpu):
+    import numpy as np
+    from collections import defaultdict
+    total = defaultdict(float)
+    for k,v in outs_cpu.items():
+        target, rep, i, ncpu = k
+        total[target, ncpu] += v['wallclock']
+    total = dict(total)
+
+    times = {}
+    frac = {}
+    for s in set(s for s,_ in total):
+        cp = list(set(c for sc,c in total if sc == s))
+        cp.sort()
+        times[s]  = np.array([total[s, c] for c in cp])
+        frac[s] = times[s][0]/times[s]
+    return times, frac
+
 
 NCPU = 8
 NREPLICATES = 3
+CPU_NREPLICATES = 3
 NSAMPLES = 3
 
 outs = {}
@@ -92,22 +112,30 @@ for rep in range(NREPLICATES):
 
 save_to(outs, '../data/precomputed/ngless_benchmark.tsv')
 
-for rep in range(NREPLICATES):
+for rep in range(CPU_NREPLICATES):
     barrier()
     prev = {'replicate': rep, 'vary-cpu': True}
-    for ncpu in [1, 2, 4, 8, 12, 16, 20, 24, 28, 32]:
+    for ncpu in [1]: # , 2, 4, 6, 8, 12, 16, 20, 24, 28, 32]:
         cleanup_ngless(prev)
         for target in [ 'gut0-rtf.ngl',
+                        'gut1-s.hg19.ngl',
                         'gut2-f.hg19.ngl',
+                        'gut4-s.igc.ngl',
                         'gut5-f.igc.ngl',
                         'gut6-p.igc.ngl',
+                        'gut.ngl',
 
                         'ocean0-rtf.ngl',
+                        'ocean1-s-om-rgc.ngl',
                         'ocean2-f-om-rgc.ngl',
                         'ocean3-p-functional-om-rgc.ngl',
                         'ocean4-p-seqname-om-rgc.ngl',
+                        'ocean.ngl',
                         ]:
             for i in range(NSAMPLES):
                 c = run_time(target, ncpu, prev)
                 outs_cpu[target, rep, i, ncpu] = parse_output(c)
                 prev = c
+            prev.hash()
+
+summary = summarize(outs_cpu)
